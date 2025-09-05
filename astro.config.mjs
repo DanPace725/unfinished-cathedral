31b91e599aa48c46662b8572cf5ec610d8e8b387
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2025-09-05 15:42:06 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server', // Enable server-side rendering
   adapter: vercel(), // Add Vercel adapter
   integrations: [
     react(), // Add React integration
   ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+ });
